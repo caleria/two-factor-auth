e8ef9090034933abcbe779b0ba393e31322b516c
Carlos Echeverria <carlosalejandroe@gmail.com>
2024-02-07 17:45:02 -0600

    -Dentro de api se crearon endpoints para login, register y logout

---
+++ api/login.php
@@ -0,0 +1,23 @@
+<?php
+
+session_start();
+
+require('../vendor/autoload.php');
+
+use App\Controllers\UserController;
+
+$rawData = file_get_contents("php://input");
+
+$data = json_decode($rawData, true);
+
+$userController = new UserController();
+
+$resultado = $userController->login($data['email'], $data['password']);
+
+if ($resultado['result']) {
+    http_response_code(200);
+    echo json_encode($resultado);
+} else {
+    http_response_code(400);
+    echo "No se puede iniciar sesion";
+}
---
+++ api/logout.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+
+require('../vendor/autoload.php');
+
+use App\Controllers\UserController;
+
+$userController = new UserController();
+$userController->logout();
+
+header('Location: ../login.php');
